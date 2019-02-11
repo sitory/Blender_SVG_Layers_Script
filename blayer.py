16f12052e014a066b1ce99c46b71316f81a5c3e7
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-02-11 00:33:52 -0200

    Update blayer.py

--- blayer.py
+++ blayer.py
@@ -1,9 +1,12 @@
 import bpy
 
 def SelectCurves():
+ Scale = (25, 25, 0)
  SelectedCurve = "Curve"
  SelectedCurveNumber = 0
  while bpy.data.objects.get(SelectedCurve) is not None:
+  bpy.data.objects[SelectedCurve].scale = Scale
+  bpy.data.objects[SelectedCurve].rotation_euler = (1.5708,0,0)
   bpy.data.objects[SelectedCurve].select = True
   SelectedCurveNumber = SelectedCurveNumber + 1
   SelectedCurve= "Curve." + str(SelectedCurveNumber).zfill(3)
@@ -13,17 +16,12 @@ def ImportSVG():
  Move = -0.001
  Layer = 0
  LayNum = 0
- Scale = (25, 25, 0)
  while bpy.data.objects.get(Object) is not None:
-  #Rotation
-  bpy.data.objects[Object].rotation_euler = (1.5708,0,0)
-  #Scale
-  bpy.data.objects[Object].scale = Scale
   bpy.data.objects[Object].location.y = Layer
   LayNum = LayNum + 1
   Layer = Layer + Move
   Object= "Curve." + str(LayNum).zfill(3)
 
 SelectCurves()
-ImportSVG()
 bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
+ImportSVG()
